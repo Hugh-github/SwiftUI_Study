5dbbf899b1eb5d9185ea9c5a00b1d356b72064e5
Hugh <102569735+Hugh-github@users.noreply.github.com>
2023-10-26 12:53:30 +0900

    docs : 폴더 생성

---
+++ SwiftUI_Study/SwiftUI_Study/InstargramClone/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  SwiftUI_Study
+//
+//  Created by dhoney96 on 2023/10/26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
