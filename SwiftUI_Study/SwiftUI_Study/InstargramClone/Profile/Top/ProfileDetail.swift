acfa74d8eb0dcf9116f4ee5b8e27778b32b835e3
Hugh <102569735+Hugh-github@users.noreply.github.com>
2023-10-27 00:37:51 +0900

    docs : 폴더 분리 및 파일 제거

---
+++ SwiftUI_Study/SwiftUI_Study/InstargramClone/Profile/Top/ProfileDetail.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileDetail.swift
+//  SwiftUI_Study
+//
+//  Created by dhoney96 on 2023/10/26.
+//
+
+import SwiftUI
+
+struct ProfileDetail: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .center, spacing: 40) {
+                ProfileCircleImage(name: "신동헌")
+                
+                HStack(spacing: 40) {
+                    Follower(count: 0, contentName: "게시물")
+                    Follower(count: 0, contentName: "팔로워")
+                    Follower(count: 0, contentName: "팔로잉")
+                }
+            }
+            .padding()
+            
+            HStack {
+                ProfileActionButton(title: "프로필 편집") {
+                    print("편집")
+                }
+                
+                Spacer()
+                
+                ProfileActionButton(title: "프로필 공유") {
+                    print("공유")
+                }
+            }
+            .padding([.leading, .trailing], 30)
+        }
+    }
+}
+
+struct ProfileDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileDetail()
+    }
+}
